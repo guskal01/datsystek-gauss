eea0cf8007185d4fc8e004f39719fe658a4326aa
Gustav Kalander <gustav.kalander@hotmail.se>
2022-05-12 00:34:23 +0200

    Fix bug

--- noptimizer.py
+++ noptimizer.py
@@ -2,7 +2,7 @@ import shutil
 import random
 from computer import AsmExecutionException
 
-noprow = "nop\t\t#noptimizer\n"
+nopline = "nop\t\t#noptimizer\n"
 tmp_file = "noptimize_test.s"
 best_file = "../noptimize_best.s"
 startstring = "### START"
@@ -11,15 +11,15 @@ stopstring = "### STOP"
 class StartStopMarkersException(Exception):
     pass
 
-def is_code(row):
-    row = row.strip()
-    if(len(row) == 0 or row[0] == '#'):
+def is_code(line):
+    line = line.strip()
+    if(len(line) == 0 or line[0] == '#'):
         return False
     return True
 
 def modify_code(code):
     starts = 0
-    while(code[0] == noprow):
+    while(code[0] == nopline):
         code.pop(0)
         starts += 1
 
@@ -31,20 +31,20 @@ def modify_code(code):
         starts = (starts+random.randint(1, 7))%8  # must change
     elif(rnd < 0.4):
         # Add one random nop
-        row = -1
-        while(row == -1 or (row != len(code) and not is_code(code[row]))):
-            row = random.randint(1, len(code))
-        code.insert(row, noprow)
+        line = -1
+        while(line == -1 or (line != len(code) and not is_code(code[line]))):
+            line = random.randint(1, len(code))
+        code.insert(line, nopline)
         if(random.random() < 0.2):
             # Move it from start instead
             starts = (starts-1)%8
     elif(rnd < 0.5):
         # Remove one random nop
-        if(code.count(noprow) > 0):
-            row = -1
-            while(row == -1 or code[row] != noprow):
-                row = random.randint(0, len(code)-1)
-            code.pop(row)
+        if(code.count(nopline) > 0):
+            line = -1
+            while(line == -1 or code[line] != nopline):
+                line = random.randint(0, len(code)-1)
+            code.pop(line)
             if(random.random() < 0.2):
                 # Move it to start instead
                 starts = (starts+1)%8
@@ -52,35 +52,35 @@ def modify_code(code):
             no_change = True
     else:
         # Move a nop
-        if(code.count(noprow) > 0):
-            row = -1
-            while(row == -1 or code[row] != noprow):
-                row = random.randint(0, len(code)-1)
-            code.pop(row)
-            assert row == len(code) or is_code(code[row])
-            nrow = row
+        if(code.count(nopline) > 0):
+            line = -1
+            while(line == -1 or code[line] != nopline):
+                line = random.randint(0, len(code)-1)
+            code.pop(line)
+            assert line == len(code) or is_code(code[line])
+            nline = line
             options_down = []
-            while nrow < len(code) and len(options_down) < 6:
-                nrow += 1
-                while(nrow < len(code) and not is_code(code[nrow])):
-                    nrow += 1
+            while nline < len(code) and len(options_down) < 6:
+                nline += 1
+                while(nline < len(code) and not is_code(code[nline])):
+                    nline += 1
                 if(random.random() < 0.5 ** len(options_down)):
-                    options_down.append(nrow)
-            nrow = row
+                    options_down.append(nline)
+            nline = line
             options_up = []
-            while nrow > 1 and len(options_up) < 6:
-                nrow -= 1
-                while(nrow > 1 and not is_code(code[nrow])):
-                    nrow -= 1
-                if(random.random() < 0.5 ** len(options_up)):
-                    options_up.append(nrow)
-            row = random.choice(options_down + options_up)
-            code.insert(row, noprow)
+            while nline > 1 and len(options_up) < 6:
+                nline -= 1
+                while(nline > 1 and not is_code(code[nline])):
+                    nline -= 1
+                if(is_code(code[nline]) and random.random() < 0.5 ** len(options_up)):
+                    options_up.append(nline)
+            line = random.choice(options_down + options_up)
+            code.insert(line, nopline)
         else:
             no_change = True
 
     for i in range(starts):
-        code.insert(0, noprow)
+        code.insert(0, nopline)
 
     if(no_change or random.random() > 0.9):
         # Another change
@@ -92,7 +92,7 @@ def noptimize(file, computer):
 
     stripped = [line.strip() for line in original]
     if(stripped.count(startstring) != 1 or stripped.count(stopstring) != 1):
-        raise StartStopMarkersException(f"Need start and stop markers. Add rows \"{startstring}\" and \"{stopstring}\" around eliminate function.")
+        raise StartStopMarkersException(f"Need start and stop markers. Add lines \"{startstring}\" and \"{stopstring}\" around eliminate function.")
     start = stripped.index(startstring)
     stop = stripped.index(stopstring)
     if(start >= stop):
